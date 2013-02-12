a9f0d98eecb9f6f39fc418276a780da8e37e4a67
Jorge Pedret <jorge@chloi.io>
2013-02-11 22:16:35 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-viewportsize
-============
+Viewport Size
+=============
 
-Simple script to display viewport size
+Drop this tag anywhere in your markup to see the width and height of your window viewport in console and bottom right of the screen.
+
+````
+<script src="viewportsize.js"></script>
+````
+or the minified version
+````
+<script src="viewportsize.min.js"></script>
+````
+
+Useful for figuring out and debugging CSS media queries when creating responsive websites.
+
+Width and height values update when you resize the browser window.
---
+++ viewportsize.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  var timeout;
+  (window.onresize = function () {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      
+      var width = document.width,
+        height = document.height,
+        markup;
+      
+      console.log(width, height);
+      
+      markup = document.createElement("div");
+      markup.innerHTML = "w: " + width + ", h: " + height;
+      markup.style.background = "#FFFFCD";
+      markup.style.color = "#444";
+      markup.style.padding = "0px 5px";
+      markup.style.position = "fixed";
+      markup.style.bottom = "5px";
+      markup.style.right = "10px";
+      markup.style.fontSize = "11px";
+      document.body.appendChild(markup);
+    }, 100);
+  })();
+}
