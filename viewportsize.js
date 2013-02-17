f1d91ca0e7a1ccb077893386bc42d9f5af446385
Jorge Pedret <jorge@chloi.io>
2013-02-16 16:31:31 -0800

    Fixed bug where it was creating a node for each resize. Now it creates it the first time and then it simply updates the innerHTML

--- viewportsize.js
+++ viewportsize.js
@@ -9,17 +9,23 @@ window.onload = function () {
         markup;
       
       console.log(width, height);
-      
-      markup = document.createElement("div");
-      markup.innerHTML = "w: " + width + ", h: " + height;
-      markup.style.background = "#FFFFCD";
-      markup.style.color = "#444";
-      markup.style.padding = "0px 5px";
-      markup.style.position = "fixed";
-      markup.style.bottom = "5px";
-      markup.style.right = "10px";
-      markup.style.fontSize = "11px";
-      document.body.appendChild(markup);
+
+      var el = document.querySelector('.viewport-size');
+      if (!el) {
+        markup = document.createElement("div");
+        markup.className = "viewport-size";
+        markup.innerHTML = "w: " + width + ", h: " + height;
+        markup.style.background = "#FFFFCD";
+        markup.style.color = "#444";
+        markup.style.padding = "0px 5px";
+        markup.style.position = "fixed";
+        markup.style.bottom = "5px";
+        markup.style.right = "10px";
+        markup.style.fontSize = "11px";
+        document.body.appendChild(markup);
+      } else {
+        el.innerHTML = "w: " + width + ", h: " + height;
+      }
     }, 100);
   })();
 }
